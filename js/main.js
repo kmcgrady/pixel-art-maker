54f51967e564dae10a9a176e622c8d134f3604b8
Kevin Meldau <me@kevinmeldau.com>
2020-12-16 23:28:08 -0500

    Add js file and edit color hex codes

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// Set all colors on color palette
+// addEventListener to the current color PublicKeyCredential. When user clicks on color from color palette the current color changes to that color
+// Add even listener to each cell so that when the user clicks on the cell, it changes to the current color
