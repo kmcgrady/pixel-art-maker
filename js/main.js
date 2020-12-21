c9fab22511517f7d518041cb0f74575302fadd5f
Kevin Meldau <me@kevinmeldau.com>
2020-12-21 15:14:52 -0600

    Did some stuff, can't remember what

--- js/main.js
+++ js/main.js
@@ -1,3 +1,2 @@
-// Set all colors on color palette
 // addEventListener to the current color PublicKeyCredential. When user clicks on color from color palette the current color changes to that color
 // Add even listener to each cell so that when the user clicks on the cell, it changes to the current color
